9561469328cb29dd16f1609a9d452a416ca27e51
JinYuRin <951996234@qq.com>
2021-06-27 20:32:18 +0800

    路由封装2

--- src/common/config/router.js
+++ src/common/config/router.js
@@ -2,7 +2,7 @@ let routes = [{
         path: '/',
         name: 'layout',
         redirect: {
-            name:'index'//默认重定向到name为index的路由去
+            name: 'index' //默认重定向到name为index的路由去
         },
         component: () => import("../../views/layout.vue"),
         // ./是查找与本文件同级目录的文件或者子文件夹
@@ -18,6 +18,13 @@ let routes = [{
         path: '/login',
         name: 'login',
         component: () => import("../../views/login/index.vue")
+    },
+    {
+        path: '*',
+        redirect: {
+            name: 'index'
+            //会有路由优先匹配规则,不符合以上的路由全部进到这里的重定向
+        },
     }
 ]
 export default routes
