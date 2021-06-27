7ce52e8133eefcc86b9a2a2ea71bd3b4bf9d42e5
JinYuRin <951996234@qq.com>
2021-06-27 20:17:10 +0800

    路由封装1

--- src/common/config/router.js
+++ src/common/config/router.js
@@ -1,6 +1,9 @@
 let routes = [{
         path: '/',
         name: 'layout',
+        redirect: {
+            name:'index'//默认重定向到name为index的路由去
+        },
         component: () => import("../../views/layout.vue"),
         // ./是查找与本文件同级目录的文件或者子文件夹
         //  /是查找本文件所在文件夹的文件或者子文件夹
