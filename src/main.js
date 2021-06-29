54d620e11bf841b80ab6e26db3885f346b348dcc
JinYuRin <951996234@qq.com>
2021-06-29 22:02:14 +0800

    doc:添加部分注释

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+/*  ctrl+alt+i
+ * @Author: mikey.zhaopeng 
+ * @Date: 2021-06-29 21:54:55 
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2021-06-29 22:01:44
+ */
 import Vue from 'vue'
 import App from './App.vue'
 import './plugins/element.js'
