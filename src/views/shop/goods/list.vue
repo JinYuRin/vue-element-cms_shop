299544f4affb9749269647656a81484fbca768e9
JinYuRin <951996234@qq.com>
2021-07-30 14:42:20 +0800

    商品列表表格UI建立

--- src/views/shop/goods/list.vue
+++ src/views/shop/goods/list.vue
@@ -11,9 +11,43 @@
           <el-tab-pane label="已下架" name="third"></el-tab-pane>
           <el-tab-pane label="库存预警" name="fourth"></el-tab-pane>
           <el-tab-pane label="回收站" name="fifth"></el-tab-pane>
-        </el-tabs></el-header
-      >
-      <el-main>Main</el-main>
+          <el-tab-pane label="全部" name="sixth"></el-tab-pane> </el-tabs
+      ></el-header>
+      <el-main>
+        <el-row type="flex" class="row-bg" justify="space-between">
+          <el-col :span="8">
+            <el-button size="mini" type="success">发布商品</el-button>
+            <el-button size="mini" type="danger">批量删除</el-button>
+            <el-button size="mini">上架</el-button>
+            <el-button size="mini">下架</el-button>
+          </el-col>
+          <el-col :span="6">
+            <el-input
+              size="medium"
+              style="width: 45%; margin-right: 10px"
+              v-model="input"
+              placeholder="要搜索的商品名称"
+            ></el-input>
+            <el-button size="mini" type="info">搜索</el-button>
+            <el-button size="mini">高级搜索</el-button>
+          </el-col>
+        </el-row>
+        <el-table class="mt-3" :data="tableData" border style="width: 100%">
+          <el-table-column prop="date" label="商品" width="180">
+          </el-table-column>
+          <el-table-column prop="name" label="实际销量" width="180">
+          </el-table-column
+          ><el-table-column prop="name" label="商品状态" width="180">
+          </el-table-column
+          ><el-table-column prop="name" label="审核状态" width="180">
+          </el-table-column
+          ><el-table-column prop="name" label="总库存" width="180">
+          </el-table-column
+          ><el-table-column prop="name" label="价格(元)" width="180">
+          </el-table-column>
+          <el-table-column prop="address" label="操作"> </el-table-column>
+        </el-table>
+      </el-main>
       <el-footer>Footer</el-footer>
     </el-container>
   </div>
@@ -22,7 +56,31 @@
 <script>
 export default {
   data() {
-    return { activeName: "second" };
+    return {
+      activeName: "second",
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄",
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄",
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄",
+        },
+      ],
+    };
   },
 
   components: {},
@@ -55,8 +113,8 @@ export default {
 .el-main {
   /* background-color: #e9eef3; */
   color: #333;
-  text-align: center;
-  line-height: 160px;
+  /* text-align: center; */
+  /* line-height: 160px; */
 }
 
 body > .el-container {
